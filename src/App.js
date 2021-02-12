7db6378566ad4c383cf96595e06fba3e4eac15c1
noordseoostende <voropayev1@mail.ru>
2021-02-12 19:08:02 +0100

    hier ik zal de 2e deel aanpassen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+// eslint-disable-next-line no-unused-vars
+import react from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const addUserHandler = () => {
+    const userData = {
+      email:'user3@gmail.com',
+      password:'testing123'
+    }
+
+    axios({
+      url:'http://localhost:5000//graphql',
+      method:'POST',
+      data:{
+        query:`
+        mutation {
+          addUser(userinput:{
+            email:"${userData.email}"
+            password:"${userData.password}"
+          }){
+            _id
+            email
+            password
+          }
+        }
+        `
+      }
+    }).then( response =>{
+      console.log(response.data)
+    }).catch( err => {
+      console.log(err)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addUserHandler}>
+
+
+      Voeg gebruiker toe
+      </button>
     </div>
   );
 }
